e47a186b153a85f50c44507f74b0e19ebe9db2a1
Aman Sharma <amans7990@gmail.com>
2020-12-03 21:12:45 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import "./App.css";
-import Routes from "./components/Header";
+import React from "react";
+import "./pages/Style.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <Router>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/about" className="link">
+          About
+        </Link>
+        <Link to="/contact" className="link">
+          Contact
+        </Link>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
