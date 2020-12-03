3db0299844bc2af2888e283f8d17d34276d9122a
Aman Sharma <amans7990@gmail.com>
2020-12-03 20:34:58 +0530

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
