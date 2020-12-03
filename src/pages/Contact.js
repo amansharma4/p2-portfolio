978b4108a63c7bc2d5536cdaec7c54379c823a6f
Aman Sharma <amans7990@gmail.com>
2020-12-03 12:09:34 +0530

    up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,35 +7,39 @@ import pic from "../img/pic.jpeg";
 
 const Contact = () => {
   return (
-    <div class="content">
-      <img class="img" src={pic} alt="profilePhoto" />
+    <div className="content">
+      <img className="img" src={pic} alt="profilePhoto" />
       <h1>Aman Sharma</h1>
-      <p class="para">
+      <p className="para">
         I am Frontend Developer upskilling the skills to be an Full Stack Dev.
       </p>
-      <h1 class="head">Skills</h1>
+      <h1 className="head">Skills</h1>
 
-      <div class="box-container">
+      <div className="box-container">
         <img
           class="imgc"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
           alt="js"
         />
-        <img class="imgc" src="https://reactjs.org/logo-og.png" alt="react" />
+        <img
+          className="imgc"
+          src="https://reactjs.org/logo-og.png"
+          alt="react"
+        />
         <img
           class="imgc"
           src="https://miro.medium.com/max/640/1*-ivYkzeuYJedPKdEdfnNlg.png"
           alt="mongodb"
         />
         <img
-          class="imgc"
+          className="imgc"
           src="https://miro.medium.com/max/2560/1*MuVcoMPyJcq8G4qf5s3HGQ.png"
           alt="nodejs"
         />
       </div>
       <br />
-      <h1 class="head">Projects</h1>
-      <div className="flex">
+      <h1 className="head">Projects</h1>
+      <div className="flex" key={1}>
         {allProject.map((post) => {
           return <Blogpost post={post} />;
         })}
@@ -48,7 +52,7 @@ const Contact = () => {
       </div>
 
       <hr />
-      <p class="para">
+      <p className="para">
         Follow my journey on
         <a href="https://twitter.com/amansharma0011" target="_">
           <span id="col"> Twitter</span>
